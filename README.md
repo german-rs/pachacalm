0c4d662955370e43c3d22a90270895b3c28eec98
german-rs <ing.germanriveros@gmail.com>
2025-03-04 17:18:58 -0300

    Se crean los directorios principales y los archivos del tema

---
+++ README.md
@@ -0,0 +1,1 @@
+#PachaCalm - AppTest
---
+++ client/lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+  );
+}
+// https://youtu.be/9gpAtzQhYkY?si=fautd2TvZtDau2i7&t=942
