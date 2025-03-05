3f215940678221d6aa93097a94a4c03d97cc595f
german-rs <ing.germanriveros@gmail.com>
2025-03-05 14:26:47 -0300

    Se avanza con la pantalla de login y widget

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,3 +1,6 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
+import 'package:client/features/auth/view/widgets/custom_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -8,8 +11,76 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign Up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomField(
+              hintText: 'Name',
+              controller: nameController,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomField(
+              hintText: 'Email',
+              controller: emailController,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomField(
+              hintText: 'Password',
+              controller: passwordController,
+              // https://youtu.be/9gpAtzQhYkY?si=kYaWYhHKTbGc8bjZ&t=2599
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            AuthGradientButton(),
+            const SizedBox(
+              height: 20,
+            ),
+            RichText(
+                text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                  TextSpan(
+                    text: 'Sign In',
+                    style: TextStyle(
+                      color: Pallete.gradient2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ])),
+          ],
+        ),
+      ),
+    );
   }
 }
