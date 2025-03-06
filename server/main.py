a86c3c43f1cf7dfda9b3cfbce21e01a4abfc585b
german-rs <ing.germanriveros@gmail.com>
2025-03-06 16:41:53 -0300

    Pruebas de backend

--- server/main.py
+++ server/main.py
@@ -1,8 +1,26 @@
+# source ./venv/bin/activate  - cambiar el entorno
+# fastapi dev main.py - ejecutar 
+# deactivate para - salir
+
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def test():
-    return 'hello world'
-# https://youtu.be/9gpAtzQhYkY?si=z6PKradW4iYVhlCo&t=3897
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+@app.post('/signup')
+def signup_user(user: UserCreate):
+    # extrac the data thats coming from rq
+    print(user.name)
+    print(user.email)
+    print(user.password)
+    # check if user already exists in db
+    # add the user to the db
+    pass
+#https://youtu.be/9gpAtzQhYkY?si=TRwIiinQL8sSGnx1&t=5026
