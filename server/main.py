3446bb928fe354577327a4d3f179c5b3ae319191
german-rs <ing.germanriveros@gmail.com>
2025-03-13 17:06:15 -0300

    Se mejora estructura de backend, login pendiente

--- server/main.py
+++ server/main.py
@@ -2,54 +2,13 @@
 # fastapi dev main.py - ejecutar 
 # deactivate para - salir
 
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-from sqlalchemy import LargeBinary, VARCHAR, TEXT, Column, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
-import bcrypt
+from fastapi import HTTPException, FastAPI
+from models.base import Base
+from routes import auth 
+from database import engine
 
 app = FastAPI()
 
-DATABASE_URL = 'postgresql://postgres@localhost:5432/pachacalmapp';
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-db = SessionLocal()
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(TEXT, primary_key = True)
-    name = Column(VARCHAR(100))
-    email = Column(VARCHAR(100))
-    password = Column(LargeBinary)
-
-@app.post('/signup')
-def signup_user(user: UserCreate):
-    # check if user already exists in db
-    user_db = db.query(User).filter(User.email == user.email).first()
-
-    if user_db:
-        return 'User with the same email already exists!'
-
-    hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
-    #https://youtu.be/9gpAtzQhYkY?si=Yx18qJ-SAKsBVglM&t=6635
-    user_db = User(id=str(uuid.uuid4()), email=user.email, password=hashed_pw, name=user.name) 
-    # add the user to the db
-    db.add(user_db)
-    db.commit()
-
-    return user_db
-
+app.include_router(auth.router, prefix='/auth')
 
 Base.metadata.create_all(engine)
